25fd388148fd60b45850d33effddf639fec4390d
Joris Mertz <info@jorismertz.nl>
2024-02-21 10:53:05 +0100

    swap out notify.nvim for noice

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -181,6 +181,17 @@ return {
       event = "InsertEnter",
     },
 
-    -- Nvim notify
-    { 'rcarriga/nvim-notify' }
+    -- Noice
+    {
+      "folke/noice.nvim",
+      event = "VeryLazy",
+      opts = {
+        -- add any options here
+      },
+      dependencies = {
+        "MunifTanjim/nui.nvim",
+        -- "rcarriga/nvim-notify",
+      }
+    }
+
   }
