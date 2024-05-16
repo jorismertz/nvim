8725c43870265848bfd027a5a5815d5fe07462f7
Joris Mertz <info@jorismertz.nl>
2024-05-16 12:59:59 +0200

    feat: support for non-macro sqlx highlights

--- queries/rust/injections.scm
+++ queries/rust/injections.scm
@@ -13,3 +13,19 @@
     )
   )
 
+
+; Injects SQL highlighting into `sqlx::query` invocations
+(field_expression
+  (call_expression
+    (scoped_identifier
+      path: (identifier) @path (#eq? @path "sqlx")
+      name: (identifier) @name (#eq? @name "query")) 
+    (arguments
+      (raw_string_literal
+        (string_content) @injection.content
+        (#set! injection.language "sql")
+      )
+    )
+  ) 
+) 
+
