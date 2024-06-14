c24daff879ff4c320220baced8f842e4499cc2c3
Joris Mertz <info@jorismertz.nl>
2024-06-14 13:01:52 +0200

    add toml formatter

--- lua/djor/plugins/conform.lua
+++ lua/djor/plugins/conform.lua
@@ -1,25 +1,29 @@
 return {
-  'stevearc/conform.nvim',
-  event = "BufRead",
-  config = function()
-    require('conform').setup({
-      format_on_save = {
-        timeout_ms = 500,
-        lsp_fallback = true,
-      },
-      formatters_by_ft = {
-        lua = { "stylua" },
-        javascript = { { "prettierd", "prettier" } },
-        typescript = { { "prettierd", "prettier" } },
-        typescriptreact = { "rustywind", { "prettierd", "prettier", } },
-        go = { "gofumpt" },
-        markdown = { "markdownlint" },
-        python = { "ruff_format" },
-        rust = { "rustfmt" },
-        handlebars = { "prettier" }
-      },
-      formatters = {
-      }
-    })
-  end
+	"stevearc/conform.nvim",
+	event = "BufRead",
+	opts = {
+		format_on_save = {
+			timeout_ms = 500,
+			lsp_fallback = true,
+		},
+		formatters_by_ft = {
+			lua = { "stylua" },
+			javascript = { { "prettierd", "prettier" } },
+			typescript = { { "prettierd", "prettier" } },
+			typescriptreact = { "rustywind", { "prettierd", "prettier" } },
+			go = { "gofumpt" },
+			markdown = { "markdownlint" },
+			python = { "ruff_format" },
+			rust = { "rustfmt" },
+			handlebars = { "prettier" },
+			toml = { "taplo" },
+		},
+		formatters = {
+			taplo = {
+				command = "taplo",
+				args = { "fmt", "--option", "align_entries=true", "$FILENAME" },
+				stdin = false,
+			},
+		},
+	},
 }
