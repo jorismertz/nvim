3cb4b6c8b20b05091d0a2fe3c47c8922d7991208
Joris Mertz <info@jorismertz.nl>
2024-06-14 13:03:17 +0200

    clean up

--- lua/djor/plugins/noice.lua
+++ lua/djor/plugins/noice.lua
@@ -1,24 +1,19 @@
 return {
-  "folke/noice.nvim",
-  event = "VeryLazy",
-  opts = {},
-  dependencies = {
-    "MunifTanjim/nui.nvim",
-  },
-  config = function()
-    require("noice").setup({
-      lsp = {
-        hover = {
-          enabled = false
-        },
-        signature = {
-          enabled = false
-        },
-        presets = {
-          long_message_to_split = true,
-          lsp_doc_border = true,
-        },
-      }
-    })
-  end
+	"folke/noice.nvim",
+	dependencies = { "MunifTanjim/nui.nvim" },
+	event = "VeryLazy",
+	opts = {
+		lsp = {
+			hover = {
+				enabled = false,
+			},
+			signature = {
+				enabled = false,
+			},
+			presets = {
+				long_message_to_split = true,
+				lsp_doc_border = true,
+			},
+		},
+	},
 }
